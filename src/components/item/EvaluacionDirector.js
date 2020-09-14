df584f1b091f9ec17b85ee72bd75eb9fbdc1ea82
Pedro Izaguirre <yuukan@gmail.com>
2020-09-14 11:34:21 -0600

    Evaluacion Director no objeció check list

--- src/components/item/EvaluacionDirector.js
+++ src/components/item/EvaluacionDirector.js
@@ -6,11 +6,21 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import ListadoDocumentosOfertas from './ListadoDocumentosOfertas';
 import ListadoDocumentosEvaluacion from './ListadoDocumentosEvaluacion';
 import ListadoDocumentosConceptoObligatorio from './ListadoDocumentosConceptoObligatorio';
+import Switch from '@material-ui/core/Switch';
 
 export default function EvaluacionDirector(props) {
     const [disabled, setDisabled] = useState(false);
     const [activity, setActivity] = useState(false);
+    const [state, setState] = useState({
+        checkedA: false,
+        checkedB: false,
+        checkedC: false,
+        checkedD: false,
+    });
 
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
 
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
@@ -29,33 +39,42 @@ export default function EvaluacionDirector(props) {
 
     //####################################Completar Información#########################################
     const complete_info = () => {
-        swal({
-            title: "¿Continuar?",
-            text: "Se solicitará la no objeción.",
-            icon: "warning",
-            buttons: ["Cancelar", "Enviar"],
-            dangerMode: true,
-        })
-            .then((certifico) => {
-                if (certifico) {
-                    let user = localStorage.getItem("bidID");
-                    setDisabled(true);
-                    axios.post(props.url + "api/solicitar-no-objecion-evaluacion",
-                        {
-                            id: props.match.params.id,
-                            user
-                        }
-                    )
-                        .then(function () {
-                            props.getProcesses();
-                            window.history.back();
-                            setDisabled(false);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                }
-            });
+        if (
+            state.checkedA
+            && state.checkedB
+            && state.checkedC
+            && state.checkedD
+        ) {
+            swal({
+                title: "¿Continuar?",
+                text: "Se solicitará la no objeción.",
+                icon: "warning",
+                buttons: ["Cancelar", "Enviar"],
+                dangerMode: true,
+            })
+                .then((certifico) => {
+                    if (certifico) {
+                        let user = localStorage.getItem("bidID");
+                        setDisabled(true);
+                        axios.post(props.url + "api/solicitar-no-objecion-evaluacion",
+                            {
+                                id: props.match.params.id,
+                                user
+                            }
+                        )
+                            .then(function () {
+                                props.getProcesses();
+                                window.history.back();
+                                setDisabled(false);
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    }
+                });
+        } else {
+            swal("Alerta", "Debe de marcar todas las preguntas", "error");
+        }
     }
     //####################################Verify#########################################
     const verify = () => {
@@ -250,10 +269,63 @@ export default function EvaluacionDirector(props) {
                         tipo={props.match.params.tipo}
                         url={props.url}
                         urlDocs={props.urlDocs}
-                        delete={false}
+                        delete={true}
                         cs_estado_proceso_id={22}
                         type={1}
                     />
+                </div>
+
+                <div className="full">
+                    <label htmlFor="checkedA" className="switch">
+                        <Switch
+                            checked={state.checkedA}
+                            onChange={handleChange}
+                            color="primary"
+                            name="checkedA"
+                            id="checkedA"
+                            inputProps={{ 'aria-label': '1. ¿La junta o comisión de evaluación se conformó conforme se establece en el ROP o MOP?' }}
+                        />
+                        1. ¿La junta o comisión de evaluación se conformó conforme se establece en el ROP o MOP?
+                    </label>
+                </div>
+                <div className="full">
+                    <label htmlFor="checkedB" className="switch">
+                        <Switch
+                            checked={state.checkedB}
+                            onChange={handleChange}
+                            color="primary"
+                            name="checkedB"
+                            id="checkedB"
+                            inputProps={{ 'aria-label': '2. ¿La evaluación de las ofertas se realizó según lo establecido en el documento de licitación y especificaciones técnicas?' }}
+                        />
+                        2. ¿La evaluación de las ofertas se realizó según lo establecido en el documento de licitación y especificaciones técnicas?
+                    </label>
+                </div>
+                <div className="full">
+                    <label htmlFor="checkedC" className="switch">
+                        <Switch
+                            checked={state.checkedC}
+                            onChange={handleChange}
+                            color="primary"
+                            name="checkedC"
+                            id="checkedC"
+                            inputProps={{ 'aria-label': '3. ¿La junta o comisión de evaluación se conformó conforme se establece en el ROP o MOP?' }}
+                        />
+                        3. ¿La junta o comisión de evaluación se conformó conforme se establece en el ROP o MOP?
+                    </label>
+                </div>
+                <div className="full">
+                    <label htmlFor="checkedD" className="switch">
+                        <Switch
+                            checked={state.checkedD}
+                            onChange={handleChange}
+                            color="primary"
+                            name="checkedD"
+                            id="checkedD"
+                            inputProps={{ 'aria-label': '4. ¿El OE está de acuerdo con el contenido del Informe de Evaluación y recomendación de adjudicación?' }}
+                        />
+                        4. ¿El OE está de acuerdo con el contenido del Informe de Evaluación y recomendación de adjudicación?
+                    </label>
                 </div>
 
                 <div className="row">                        
